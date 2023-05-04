da5b4dd01fea554d0ac1435208729a5da8089d74
shubham kumar <clboy768@gmail.com>
2023-05-05 04:59:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Content from './component/Content';
+import Item from './component/Item';
+import Navbar from './component/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Navbar/>
+      <Content/>
+     <Item/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Content.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Content.css'
+const Content = () => {
+  return (
+    <div className='content'>
+      <h1>The Generics</h1>
+    </div>
+  )
+}
+
+export default Content
